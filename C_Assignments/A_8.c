2c8dc26082aaab33d6848166ee42ccd6ceb7feff
shubhamchouhan23 <63260303+shubhamchouhan23@users.noreply.github.com>
2022-07-07 12:42:53 +0530

    Update A_8.c

--- C_Assignments/A_8.c
+++ C_Assignments/A_8.c
@@ -1,26 +1,21 @@
-/*
-Q8.C program to copy from one file to another file.
-*/
-
+//C program to copy from one file to another file.
 #include<stdio.h>
 #include<stdlib.h>
-#include<sys/types.h>
-#include<fcntl.h>
-
-int main() {
-
-    int fd1, fd2;
-
-    char reading[1000];
-
-    fd1 = open("Strings.txt", O_RDWR | O_CREAT, 0777);
-    fd2 = open("String2.txt", O_RDWR | O_CREAT, 0777);
-
-    int len = read(fd1, reading, sizeof(reading));
-    write(fd2, reading, len);
-
-    close(fd1);
-    close(fd2);
-
-    return 0;
+int main()
+{
+    char c;
+    FILE *fp1 = fopen("Strings.txt","r");
+    FILE *fp = fopen("new1.txt","w");
+    if(fp == NULL)
+    {
+        printf("can't open file");
+        exit(0);
+    }
+    while((c=fgetc(fp1))!=EOF)
+    {
+        fputc(c,fp);
+    }
+    printf("Done\n");
+    fclose(fp);
+    fclose(fp1);
 }
