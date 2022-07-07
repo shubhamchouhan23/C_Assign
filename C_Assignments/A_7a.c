9cd1e56357c46979e79b82b3c305369d02ed761f
shubhamchouhan23 <63260303+shubhamchouhan23@users.noreply.github.com>
2022-07-07 12:41:42 +0530

    Update A_7a.c

--- C_Assignments/A_7a.c
+++ C_Assignments/A_7a.c
@@ -1,21 +1,27 @@
-/*
-Q7.C program to create a file and store information.
-    
-*/
-
+/* C program to create a file and store information.*/
 #include<stdio.h>
-#include<sys/types.h>
-#include<fcntl.h>
-
-int main() {
-
-    int fd;
-
-    char buffer[100] = "Hello i am storing information in this data file. Yipeee exicted to go in file.";
-    
-    fd = creat("data.txt",  0777);
-    write(fd, buffer, sizeof(buffer));
-
-    close(fd);
-    return 0;
+#define FILENAME "new.txt"
+void main()
+{
+    FILE *fp = fopen("new.txt","w");
+    int age;
+    float sal;
+    char n[20];
+    char ch;
+    int count =0;
+    if(fp==NULL)
+    {
+        printf("file does not exists\n");
+        return;
+    }
+    printf("enter the name\n");
+    scanf("%s",n);
+    fprintf(fp,"name = %s\n",n);
+    printf("enter the age\n");
+    scanf("%d",&age);
+    fprintf(fp,"age = %d\n",age);
+    printf("enter the salary\n");
+    scanf("%f",&sal);
+    fprintf(fp,"salary = %.2f\n",sal);
+    fclose(fp);
 }
